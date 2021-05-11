fd04d36130febe8e878f6cdf30934af708b97d9a
Claudio Javier Rain Levicán <rainclaudio25@gmail.com>
2021-05-11 15:17:16 -0400

    finish auth

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -26,19 +26,21 @@ export class BookingService {
     return this._bookingsVector.asObservable();
   }
   cancelBooking(bookingId: string) {
-    return this.http
-      .delete(
-        `https://ionic-angular-course-d0193-default-rtdb.firebaseio.com/booked-places/${bookingId}.json`
-      )
-      .pipe(
-        switchMap(() => {
-          return this._bookingsVector;
-        }),
-        take(1),
-        tap((bookings) => {
-          this._bookingsVector.next(bookings.filter((b) => b.id !== bookingId));
-        })
-      );
+    return this.authService.token.pipe(
+      take(1),
+      switchMap((token) => {
+        return this.http.delete(
+          `https://ionic-angular-course-d0193-default-rtdb.firebaseio.com/booked-places/${bookingId}.json?auth=${token}`
+        );
+      }),
+      switchMap(() => {
+        return this._bookingsVector;
+      }),
+      take(1),
+      tap((bookings) => {
+        this._bookingsVector.next(bookings.filter((b) => b.id !== bookingId));
+      })
+    );
   }
   addBooking(
     placeId: string,
@@ -52,17 +54,23 @@ export class BookingService {
   ) {
     let generatedId: string;
     let newBooking: Booking;
+    let fetchedUserId: string;
     return this.authService.userId.pipe(
       take(1),
       switchMap((userId) => {
         if (!userId) {
           throw new Error('No user id found!');
         }
+        fetchedUserId = userId;
 
+        return this.authService.token;
+      }),
+      take(1),
+      switchMap((token) => {
         newBooking = new Booking(
           Math.random().toString(),
           placeId.toString(),
-          userId,
+          fetchedUserId,
           placeTitle,
           placeImage,
           firstname,
@@ -72,7 +80,7 @@ export class BookingService {
           dateTo
         );
         return this.http.post<{ name: string }>(
-          'https://ionic-angular-course-d0193-default-rtdb.firebaseio.com/booked-places.json',
+          `https://ionic-angular-course-d0193-default-rtdb.firebaseio.com/booked-places.json?auth=${token}`,
           {
             ...newBooking,
             // this replaces the current id given from the function
@@ -101,47 +109,48 @@ export class BookingService {
   }
 
   fetchBookings() {
-
+    let fetchedUserId: string;
     return this.authService.userId.pipe(
       take(1),
-      switchMap(userId => {
-        if(!userId){
+      switchMap((userId) => {
+        if (!userId) {
           throw new Error('User not found');
         }
+        fetchedUserId = userId;
+        return this.authService.token;
+
+      }),
+      take(1),
+      switchMap(token => {
         return this.http.get<{ [key: string]: BookData }>(
-        `https://ionic-angular-course-d0193-default-rtdb.firebaseio.com/booked-places.json?orderBy="userId"&equalTo="${userId}"`
+          `https://ionic-angular-course-d0193-default-rtdb.firebaseio.com/booked-places.json?orderBy="userId"&equalTo="${fetchedUserId}"&auth=${token}`
         );
-    }),
-    map((resData) => {
-          const booking = [];
-          for (const key in resData) {
-            if (resData.hasOwnProperty(key)) {
-              booking.push(
-                new Booking(
-                  key,
-                  resData[key].placeId,
-                  resData[key].userId,
-                  resData[key].placeTitle,
-                  resData[key].placeImage,
-                  resData[key].firstname,
-                  resData[key].lastname,
-                  resData[key].guestNumber,
-                  new Date(resData[key].bookedFrom),
-                  new Date(resData[key].bookedTo)
-                )
-              );
-            }
+      }),
+      map((resData) => {
+        const booking = [];
+        for (const key in resData) {
+          if (resData.hasOwnProperty(key)) {
+            booking.push(
+              new Booking(
+                key,
+                resData[key].placeId,
+                resData[key].userId,
+                resData[key].placeTitle,
+                resData[key].placeImage,
+                resData[key].firstname,
+                resData[key].lastname,
+                resData[key].guestNumber,
+                new Date(resData[key].bookedFrom),
+                new Date(resData[key].bookedTo)
+              )
+            );
           }
-          return booking;
-        }),
-        tap((bookings3) => {
-          this._bookingsVector.next(bookings3);
-        })
-      );
-    }
-
-
-
-
-
+        }
+        return booking;
+      }),
+      tap((bookings3) => {
+        this._bookingsVector.next(bookings3);
+      })
+    );
+  }
 }
